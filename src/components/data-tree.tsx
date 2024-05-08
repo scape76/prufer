f68d471915e0ba2e56d5a7d4c16fd3e0919b8195
Dan <danyaolekhq@gmail.com>
2024-05-09 00:57:16 +0300

    fix: draggin doesnt work on init render

--- src/components/data-tree.tsx
+++ src/components/data-tree.tsx
@@ -5,24 +5,34 @@ import { treeAtom } from "@/store/tree";
 import { useAtom } from "jotai";
 import sample from "@/assets/sample.json";
 import { useCenteredTree } from "@/hooks/use-centered-tree";
+import { useEffect, useState } from "react";
 
 export function DataTree() {
+  const [render, setRender] = useState(false);
   const [tree] = useAtom(treeAtom);
   const [translate, containerRef] = useCenteredTree();
 
   const treeToShow = Object.keys(tree).length === 0 ? sample : tree;
 
+  useEffect(() => {
+    setRender(true);
+  }, []);
+
   return (
     <div className="h-[calc(100vh-250px)] border mt-6" ref={containerRef}>
-      <Tree
-        orientation="vertical"
-        svgClassName="dark:bg-accent"
-        leafNodeClassName="bg-white text-white"
-        branchNodeClassName="bg-white text-white"
-        dataKey={JSON.stringify(treeToShow)}
-        translate={translate}
-        data={treeToShow}
-      />
+      {/* dragging sometimes doesn't work on initial render */}
+      {render && (
+        <Tree
+          orientation="vertical"
+          svgClassName="dark:bg-accent"
+          leafNodeClassName="bg-white text-white"
+          branchNodeClassName="bg-white text-white"
+          dataKey={JSON.stringify(treeToShow)}
+          translate={translate}
+          data={treeToShow}
+          draggable
+        />
+      )}
     </div>
   );
 }
